b34fe56f4b66f2d632205e11cd76c0cd02184f12
Aditya Kushwaha <aditya.kushwaha@xebia.com>
2024-07-10 10:59:02 +0530

    Insertion Scripts

---
+++ src/main/resources/sql/dev/V2024070910460000__INSERT_ACCOUNT.sql
@@ -0,0 +1,5 @@
+INSERT INTO account (name) VALUES ('Saving Account');
+INSERT INTO account (name) VALUES ('Business Account');
+INSERT INTO account (name) VALUES ('Checking Account');
+INSERT INTO account (name) VALUES ('Student Account');
+INSERT INTO account (name) VALUES ('Joint Account');
