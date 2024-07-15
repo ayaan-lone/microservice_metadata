3f075163dbb38c17012234f07ef5985fe01da78f
Himanshu Singh Dhapola <himanshu.dhapola@xebia.com>
2024-07-15 17:53:02 +0530

    Flyway queries added

--- src/main/resources/sql/dev/V2024070910490000__INSERT_CARD.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO card (name, account_id) VALUES ('DEBIT CARD', 1);
-INSERT INTO card (name, account_id) VALUES ('CREDIT CARD', 1);
-INSERT INTO card (name, account_id) VALUES ('PREPAID CARD', 1);
-INSERT INTO card (name, account_id) VALUES ('VIRTUAL CARD', 1);
-INSERT INTO card (name, account_id) VALUES ('REWARD CARD', 1);
---
+++ src/main/resources/sql/dev/V2024071512160000__INSERT_CARD.sql
@@ -0,0 +1,5 @@
+INSERT INTO card (name, account_id) VALUES ('DEBIT CARD', 1);
+INSERT INTO card (name, account_id) VALUES ('CREDIT CARD', 2);
+INSERT INTO card (name, account_id) VALUES ('PREPAID CARD', 3);
+INSERT INTO card (name, account_id) VALUES ('VIRTUAL CARD', 4);
+INSERT INTO card (name, account_id) VALUES ('REWARD CARD', 5);
